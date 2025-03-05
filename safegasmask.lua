911c1dbd9b5901f63623919cc52132b94778d49e
meinhard-ru <ivan.zvyagin12@gmail.com>
2025-03-05 21:39:06 +0300

    Add files via upload

---
+++ safegasmask.lua
@@ -0,0 +1,61 @@
+script_name("safegasmask")
+script_author("Isus_Christos")
+
+local sampev = require('lib.samp.events')
+
+local isDead = false
+local isActive = true
+local isActiveSafe = false
+local isGasmask = false
+
+function main()
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+
+    userNotification('Да защитит тебя безопасный противогаз! /sgm - деактивировать скрипт.')
+
+    sampRegisterChatCommand("sgm", function()
+        isActive = not isActive
+        if isActive then
+            userNotification('Скрипт вновь работает, противогаз сохраняется.')
+        else
+            userNotification('Скрипт теперь не работает. Противогаз не сохраняется.')
+        end
+    end)
+
+    while true do
+        wait(0)
+        onUserDeath()
+    end
+end
+
+function onUserDeath()
+    if isCharDead(PLAYER_PED) and not isDead and isActive then
+        lua_thread.create(function()
+            isActiveSafe = true
+            isDead = true
+            sampSendChat('/i')
+            wait(400)
+            if isGasmask then
+                userNotification('Противогаз уже в инвентаре, сохранять не требуется.')
+            elseif not isGasmask then
+                sampSendChat('/gasmask')
+                userNotification('Противогаз успешно снят и сохранен после смерти.')
+            end
+            isGasmask = false
+            isActiveSafe = false
+        end)
+    elseif not isCharDead(PLAYER_PED) and isDead then
+        isDead = false
+    end
+end
+
+function sampev.onShowTextDraw(id, data)
+    if data.modelId == 19472 and isActiveSafe then 
+        isGasmask = true
+    end
+end
+
+function userNotification(userNotificationText)
+    sampAddChatMessage("[safegasmask] {FFFFFF}"..userNotificationText, 0x9966CC)
+end
